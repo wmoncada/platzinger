4d348a4bde2145acb2b457e55857734f33088189
Walberto Moncada <monposmetal@gmail.com>
2019-03-17 18:00:56 -0600

    Sistema de routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ConversationComponent } from './conversation/conversation.component';
 import { ProfileComponent } from './profile/profile.component';
+import { Routes, RouterModule } from '@angular/router';
+
+// Rutas
+const appRoutes : Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'conversation', component: ConversationComponent },
+  { path: 'profile', component: ProfileComponent },
+];
 
 @NgModule({
   declarations: [
@@ -18,9 +28,9 @@ import { ProfileComponent } from './profile/profile.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [LoginComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
